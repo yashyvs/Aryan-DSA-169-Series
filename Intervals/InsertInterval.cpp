8c36f4116f77faeabdd8d54c6ebfa9c530a01cfe
Yash Vardhan Singh <yashvardhan.yvs21@gmail.com>
2023-07-16 23:59:50 +0530

    Create InsertInterval.cpp

---
+++ Intervals/InsertInterval.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;;
+        for(auto it:intervals){
+            if(it[1]<newInterval[0]) ans.push_back(it);
+            else if(newInterval[1] < it[0]) {
+                ans.push_back(newInterval);
+                newInterval=it;
+            }
+            else {
+                newInterval[0]=min(newInterval[0], it[0]);
+                newInterval[1]=max(newInterval[1], it[1]);
+            }    
+        }
+        ans.push_back(newInterval);
+        return ans;
+    }
+};
